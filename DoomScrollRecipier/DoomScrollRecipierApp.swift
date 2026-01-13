73a90cfeb6aa065c3553d1d95e2f1537320a7276
diego <diego.pandolfo@twinformatics.at>
2026-01-13 09:29:00 +0100

    Initial Commit

---
+++ DoomScrollRecipier/DoomScrollRecipierApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoomScrollRecipierApp.swift
+//  DoomScrollRecipier
+//
+//  Created by Pandolfo Diego on 13.01.26.
+//
+
+import SwiftUI
+
+@main
+struct DoomScrollRecipierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
